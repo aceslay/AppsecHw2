749aada2782dda96b338e1e98881d10597326d87
aceslay <kris.ace@nyu.edu>
2021-11-29 12:36:26 -0500

    part_1_complete

--- GiftcardSite/GiftcardSite/settings.py
+++ GiftcardSite/GiftcardSite/settings.py
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sslserver',
+    'django_extensions',
+    
     
     
     # Custom
